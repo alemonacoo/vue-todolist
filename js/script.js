d30b2db0d10da6b7453ab28aa98163bf4cd635fd
alemonacoo <ale.monaco@outlook.com>
2022-09-14 14:51:02 +0200

    Milestone 0.5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log("JS OK!");
+
+const toDoListArray = [];
+
+console.log(toDoListArray);
+
+const app = new Vue(
+    {
+        el: "#app",
+        data:{
+            toDoListArray,
+            toDoItem: undefined,
+        },
+        methods:{
+            addToDoItem(item){
+                toDoListArray.push(item);
+                console.log(toDoListArray);
+                this.toDoItem = '';
+            },
+        }
+
+    }
+)
